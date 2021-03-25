e4be7c616e727b0ea27dcc6834b14c55399bb869
Karl Broman <kbroman@gmail.com>
2021-03-25 17:43:37 -0500

    R/logo.R: put "fundamentals" on the bottom

--- R/logo.R
+++ R/logo.R
@@ -9,8 +9,8 @@ lwd <- 5
 text_cex <- 4
 rounding <- 0.2
 
-text <- c("fundamentals", "verify", "explore", "confer", "document")
-colors <- brocolors("web")[c("black", "blue", "green", "orange", "purple")]
+text <- c("verify", "explore", "confer", "document", "fundamentals")
+colors <- brocolors("web")[c("blue", "green", "orange", "purple", "black")]
 
 pdf("../Figs/logo.pdf", height=7.5, width=10, pointsize=14)
 
@@ -20,11 +20,10 @@ plot(0,0,type="n", xlab="", ylab="", xaxt="n", yaxt="n",
      bty="n", xaxs="i", yaxs="i")
 
 
-w <- c(width*2+gap, rep(width, 4))
+w <- c(rep(width, 4), width*2+gap)
 h <- rep(height, 5)
-x <- c(gap*0.5, rep(c(gap*0.5, gap*1.5 + width), 2))
-y <- c(height*2 + gap*2.5, height + gap*1.5, height + gap*1.5,
-       gap*0.5, gap*0.5)
+x <- c(rep(c(gap*0.5, gap*1.5 + width), 2), gap*0.5)
+y <- c(rep(2*height + gap*2.5, 2), rep(height+gap*1.5, 2), gap*0.5)
 
 for(i in 1:5) {
     roundedRect(x[i], y[i], x[i]+w[i], y[i]+h[i],
